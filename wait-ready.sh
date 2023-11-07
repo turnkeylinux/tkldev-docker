c55748633d9e4ac2b3692752b721c7f35fa9aab0
Stefan Davis <stefan@turnkeylinux.org>
2023-11-07 23:37:01 +0000

    add wait-ready script

---
+++ wait-ready.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+CONT_ID="$1"
+
+if [[ -z $CONT_ID ]]; then
+    echo "usage: $0 <container-id>" >&2
+    exit 1
+fi
+
+COM=
+
+if command -v docker; then
+    COM=docker
+elif command -v podman; then
+    COM=podman
+else
+    echo "requires either docker or podman" >&2
+    exit 1
+fi
+
+count=0
+while true; do
+    if ((count > 40)); then
+        echo "didn't start in time" >&2
+        exit 1
+    fi
+    ((count++))
+    if "$COM" exec "$("$COM" ps | grep tkl/ | cut -d' ' -f1)" grep 'run completed' /var/log/inithooks.log; then
+        exit 0
+    fi
+    echo "-"
+    sleep 10
+done
